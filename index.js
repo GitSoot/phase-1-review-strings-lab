77494ddaea3e4fb96122e3c39e4105458ab2a93b
Eric Onyango <eric.onyango@student.moringaschool.com>
2022-11-27 21:08:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Anyone Anyona';
+const welcomeMessage = 'Welcome to Flatbook, Anyone Anyona!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ANYONE ANYONA!'
+const firstInitial = currentUser [0];
+const restOfName = currentUser.slice(1);
+const shortGreeting = `Welcome, ${firstInitial}!`
